56c5b5bf21e115bcdf959422f5dc06682b3afc24
Neil Comber <neilcomber@Neils-MacBook-Pro.local>
2024-01-24 10:59:20 +0000

    review model added

--- models/campground.js
+++ models/campground.js
@@ -6,7 +6,12 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number, 
     description: String, 
-    location: String
+    location: String, 
+    reviews: [
+        {
+            type: Schema.Types.ObjectId, 
+            ref: 'Review'
+        }]
 })
 
 
