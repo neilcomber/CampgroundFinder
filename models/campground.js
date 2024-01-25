1d1c085ced3390c41f5867c9ed5bafb4e1729a85
Neil Comber <neilcomber@Neils-MacBook-Pro.local>
2024-01-25 10:01:43 +0000

    deleting middleware for mongoose

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -12,6 +13,16 @@ const CampgroundSchema = new Schema({
             type: Schema.Types.ObjectId, 
             ref: 'Review'
         }]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 
