2696bd466cee2ee5ced70aba866e68381665393c
Neil Comber <106420893+neilcomber@users.noreply.github.com>
2024-03-05 18:08:05 +0000

    fix port issue

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ app.use((err, req, res, next) => {
 })
 
 
-const port = process.env.POR || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
     console.log(`serving on port ${port}`)
