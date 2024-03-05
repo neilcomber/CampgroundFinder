73b159084d0e9046912cba7b17f2f6d48aec6213
Neil Comber <106420893+neilcomber@users.noreply.github.com>
2024-03-05 16:25:47 +0000

    added start script

--- app.js
+++ app.js
@@ -174,7 +174,8 @@ app.use((err, req, res, next) => {
 })
 
 
+const port = process.env.POR || 3000;
 
-app.listen(3000, ()=>{
-    console.log('serving on port 3000')
+app.listen(port, ()=>{
+    console.log(`serving on port ${port}`)
 })
